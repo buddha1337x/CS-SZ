6b321388684ac3cba7c8512984c969ff8ec3addb
buddha1337x <karsunog.business@gmail.com>
2025-02-21 23:30:07 -0500

    Initial Push of CS-SZ

---
+++ client/client.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        print("Hello from the client!")
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'carson - Classic Scripts'
+version '1.0.0'
+lua54 'yes'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+files {
+    'html/index.html'
+}
