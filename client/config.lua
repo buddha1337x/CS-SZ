0b64160dad722e9f1c5c7e8af71267df8efb6214
buddha1337x <karsunog.business@gmail.com>
2025-02-22 01:27:25 -0500

    Updated configs

--- client/config.lua
+++ client/config.lua
@@ -7,15 +7,12 @@ Config.SafezoneMeleeDisable = true          -- Disallow melee/punching in a safe
 Config.SafezoneSpeedLimit = 20.0            -- Speed limit (m/s) within a safezone
 Config.SafezoneZRange = 10.0                -- Vertical tolerance when checking if inside safezone
 
--- Marker settings (for drawing safezone boundaries)
-Config.SafezoneDisplayMarker = true
+-- Marker & Wall settings (for drawing safezone boundaries in admin outline mode)
+Config.SafezoneDisplayMarker = false        -- Default: outlines are hidden (admins toggle with /safezones)
 Config.SafezoneMarkerColor = { r = 0, g = 255, b = 0, a = 150 }
+Config.SafezoneWallHeight = 10.0            -- Height of the wall along each safezone edge
+Config.SafezoneWallColor = { r = 0, g = 200, b = 0, a = 150 }
 
 -- Admin settings for safezone creation/deletion
 Config.AdminCommand = "createsafezone"      -- Command to start safezone creation mode
 Config.AdminDeleteCommand = "deletesafezone"  -- Command to delete the nearest safezone
-
--- Camera & creation mode settings
-Config.SafezoneCameraHeight = 15.0          -- Height above the player for the creation camera
-Config.SafezoneMarkKey = 38                 -- Key to mark a safezone point (38 = E key)
-Config.SafezoneCreationInstructions = "Press ~INPUT_CONTEXT~ to mark a point for the safezone."
---
+++ server/config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+-- (You can reuse the same safezone enforcement settings as in client/config.lua)
+Config.SafezoneZRange = 10.0
