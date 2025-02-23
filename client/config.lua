39b5bf9a1ea2bc986a3a590127396370698e1909
buddha1337x <karsunog.business@gmail.com>
2025-02-23 14:43:32 -0500

    Updated UI

--- client/config.lua
+++ client/config.lua
@@ -1,18 +1,17 @@
 Config = {}
 
--- Safezone enforcement settings
 Config.SafezoneMessage = "You are in a safezone."
-Config.SafezoneWeaponDisable = true       -- Disallow firing weapons in a safezone
-Config.SafezoneMeleeDisable = true          -- Disallow melee/punching in a safezone
-Config.SafezoneSpeedLimit = 20.0            -- Speed limit (m/s) within a safezone
-Config.SafezoneZRange = 10.0                -- Vertical tolerance when checking if inside safezone
+Config.SafezoneWeaponDisable = true      
+Config.SafezoneMeleeDisable = true         
+Config.SafezoneSpeedLimit = 20.0            
+Config.SafezoneZRange = 10.0                
 
--- Marker & Wall settings (for drawing safezone boundaries in admin outline mode)
-Config.SafezoneDisplayMarker = false        -- Default: outlines are hidden (admins toggle with /safezones)
+
+Config.SafezoneDisplayMarker = false        
 Config.SafezoneMarkerColor = { r = 0, g = 255, b = 0, a = 150 }
-Config.SafezoneWallHeight = 10.0            -- Height of the wall along each safezone edge
+Config.SafezoneWallHeight = 10.0            
 Config.SafezoneWallColor = { r = 0, g = 200, b = 0, a = 150 }
 
--- Admin settings for safezone creation/deletion
-Config.AdminCommand = "createsafezone"      -- Command to start safezone creation mode
-Config.AdminDeleteCommand = "deletesafezone"  -- Command to delete the nearest safezone
+
+Config.AdminCommand = "createsafezone"
+Config.AdminDeleteCommand = "deletesafezone"
